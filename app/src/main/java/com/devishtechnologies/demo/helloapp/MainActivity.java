8aa27125425b6b55f25203eb6d26d1d1e7acb87d
Vijay Kumar <futureinjava@gmail.com>
2018-07-08 16:13:30 +0530

    hello button example with linear layout

---
+++ app/src/main/java/com/devishtechnologies/demo/helloapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.devishtechnologies.demo.helloapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void sayHello(View view){
+        Toast.makeText(this,  "Jai Shree Ram", Toast.LENGTH_SHORT).show();
+
+    }
+}
